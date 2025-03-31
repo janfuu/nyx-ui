d8464d65a061d1f52f66ead2f7f7cc37f7b58d3a
jan.fuchsmann@gmail.com <jan.fuchsmann@gmail.com>
2025-03-31 20:54:08 +0000

    Edited Form2

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -13,6 +13,6 @@ class Form2(Form2Template):
 
     # Any code you write here will run before the form opens.
 
-  def button_1_click(self, **event_args):
+  def btn_return_click(self, **event_args):
     """This method is called when the button is clicked"""
     pass
